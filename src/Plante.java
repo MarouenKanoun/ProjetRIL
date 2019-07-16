2a1b86ab817491375ad510364273899693b4e3c2
unknown <marouen1931@hotmail.com>
2019-07-16 12:20:56 +0200

    commit 4

--- src/Plante.java
+++ src/Plante.java
@@ -1,4 +1,47 @@
 
 public class Plante {
+	
+	
+
+	private Boolean EstVivante;
+	private String NomPlante;
+	private String ViePlante;
+	private String DegasPlante;
+	private String VitesseAttaquePlante;
+	
+	
+
+	public Boolean getEstVivante() {
+		return EstVivante;
+	}
+	public void setEstVivante(Boolean estVivante) {
+		EstVivante = estVivante;
+	}
+	public String getNomPlante() {
+		return NomPlante;
+	}
+	public void setNomPlante(String nomPlante) {
+		NomPlante = nomPlante;
+	}
+	public String getViePlante() {
+		return ViePlante;
+	}
+	public void setViePlante(String viePlante) {
+		ViePlante = viePlante;
+	}
+	public String getDegasPlante() {
+		return DegasPlante;
+	}
+	public void setDegasPlante(String degasPlante) {
+		DegasPlante = degasPlante;
+	}
+	public String getVitesseAttaquePlante() {
+		return VitesseAttaquePlante;
+	}
+	public void setVitesseAttaquePlante(String vitesseAttaquePlante) {
+		VitesseAttaquePlante = vitesseAttaquePlante;
+	}
+	
+	
 
 }
