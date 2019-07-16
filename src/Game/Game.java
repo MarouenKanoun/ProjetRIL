914a3c55ea4f5c4386171ff4ffcbf3e753dd182a
FINAUD GUILLAUME <guillaume.finaud@viacesi.fr>
2019-07-16 11:21:25 +0200

    start_zombie

---
+++ src/Game/Game.java
@@ -0,0 +1,11 @@
+package game;
+
+public interface Game
+{
+    public void start();
+    public void create();
+    public void restore();
+    public void save();
+    public void suspend();
+    public void resume();
+}
---
+++ src/plantsVSZombies/zombie.java
@@ -0,0 +1,7 @@
+package plantsVSZombies;
+
+public class zombie {
+	private final String zombi_image = "image/zombie_base.png";
+	private final int zombi_size = 32;
+	private final int zombi_drow_size = 48;
+}
