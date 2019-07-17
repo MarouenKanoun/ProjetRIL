5ce8760b40fb7ca81328ce3e5b503fb35c2f1ef5
unknown <marouen1931@hotmail.com>
2019-07-17 11:25:28 +0200

    Grid

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,14 +1,32 @@
-package game;
+package Game;
 
+import java.awt.*;
+import javax.swing.*;
 
-public interface Game
-{
-    public void start();
-    public void create();
-    public void restore();
-    public void save();
-    public void suspend();
-    public void resume();
-    
-    
+public class Game extends JFrame{
+    public Game(){
+        JPanel container = new JPanel();
+        int largeurMax = 7;
+        int longueurMax = 11;
+        this.setTitle("Grid Layout");
+        this.setSize(1600, 900);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        container.setLayout(new GridLayout(largeurMax,longueurMax));
+        for (int i=0; i<largeurMax; i++){
+            for (int j=0; j<longueurMax; j++){
+                JButton jb = new JButton();
+                container.add(jb);
+                jb.setBackground(new Color(0x64FF64));
+                if (i == 0 || i == largeurMax-1 || j ==0 || j == longueurMax-1) {
+                    jb.setBackground(new Color(0x00FF00));
+                }
+            }
+        }
+        this.setContentPane(container);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Game gl = new Game();
+    }
 }
