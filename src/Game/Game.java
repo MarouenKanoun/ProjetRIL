b2e9338e2eafd5b6cb92e2177d2c83ad7f302f1c
FINAUD GUILLAUME <guillaume.finaud@viacesi.fr>
2019-07-18 10:45:47 +0200

    add tour

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,100 +6,118 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.*;
 
 public class Game extends JFrame {
 	public static JButton BoutonMemoire = null;
+	public static JButton BoutonTour = null;
 	public static Plante PlanteEnMemoire = null;
 	public static CaseData[][] CaseDatas;
 	public static Zombie Zombie;
+	public static int Tour = 0;
+	public static boolean Loose = false;
 
-	public Game()
-    {
-			
-
+	public Game() {
 		
-    	CaseDatas = new CaseData [5][9];
-    	
-    	//Resize icon----------------
-    	//ImageIcon icon = new ImageIcon(new ImageIcon("image/NOIX.png").getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
-    	ImageIcon iconNoix = new ImageIcon(new ImageIcon("image/NOIX.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));    
-    	ImageIcon iconPoire = new ImageIcon(new ImageIcon("image/poire.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
-    	ImageIcon iconSoleil = new ImageIcon(new ImageIcon("image/soleil.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
-    	//-----------------------
-    	//ImageIcon IC = new ImageIcon();
-        JPanel container = new JPanel();
-        int largeurMax = 7;
-        int longueurMax = 11;
-        this.setTitle("Grid Layout");
-        this.setSize(1600, 900);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        container.setLayout(new GridLayout(largeurMax,longueurMax));
-        for (int i=0; i<largeurMax; i++)
-        {
-            for (int j=0; j<longueurMax; j++)
-            {
-                JButton jb = new JButton();
-                container.add(jb);
-                jb.setBackground(new Color(0x64FF64));
-                Boolean rentre = false;
-                if (i == 0 || i == largeurMax-1 || j ==0 || j == longueurMax-1) 
-                {
-                    jb.setBackground(new Color(0x00FF00));
-                    jb.setEnabled(false); //Le bouton n'est plus cliquable
-                	rentre = true;
-                    
-                }
-                if ( i == 0 && j ==0) 
-                {
-                	BoutonMemoire = jb;
-                	rentre = true;
-                }
-                if ( i == largeurMax-1 && j ==6) 
-                {
-                    jb.setIcon(new ImageIcon(iconNoix.getImage()));
-                    jb.setEnabled(true); 
-                	rentre = true;
-                   // JOptionPane.showMessageDialog(null, "Noix  ");
-                    clickActionGetPlante(jb,  new Noix());
-                }
-                if ( i == largeurMax-1 && j ==5) 
-                {
-                    jb.setIcon(new ImageIcon(iconPoire.getImage()));
-                    jb.setEnabled(true); 
-                	rentre = true;
-                   // JOptionPane.showMessageDialog(null, "je suis une pois  ");
-                    clickActionGetPlante(jb, new Poire());
-                }
-                if ( i == largeurMax-1 && j ==4) 
-                {
-                    jb.setIcon(new ImageIcon(iconSoleil.getImage()));
-                    jb.setEnabled(true);
-                	rentre = true; 
-                    //IC.setImage(getIconImage());
-                    //jb.addActionListener(this);
-                    clickActionGetPlante(jb, new Soleil());
-                    
-                    
-                }
-                if(!rentre) {
-                	CaseData cd = new CaseData();
-                	cd.jButton = jb;
-                	clickActionSetPlante(cd);
-                }
-            }
-            
-        }
-        this.setContentPane(container);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-    }
+		
+
+		CaseDatas = new CaseData[5][9];
+
+		// Resize icon----------------
+		// ImageIcon icon = new ImageIcon(new
+		// ImageIcon("image/NOIX.png").getImage().getScaledInstance(200, 200,
+		// Image.SCALE_DEFAULT));
+		ImageIcon iconNoix = new ImageIcon(
+				new ImageIcon("image/NOIX.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+		ImageIcon iconPoire = new ImageIcon(
+				new ImageIcon("image/poire.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+		ImageIcon iconSoleil = new ImageIcon(
+				new ImageIcon("image/soleil.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+		// -----------------------
+		// ImageIcon IC = new ImageIcon();
+		JPanel container = new JPanel();
+		int largeurMax = 7;
+		int longueurMax = 11;
+		this.setTitle("Grid Layout");
+		this.setSize(1600, 900);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		container.setLayout(new GridLayout(largeurMax, longueurMax));
+		for (int i = 0; i < largeurMax; i++) {
+			for (int j = 0; j < longueurMax; j++) {
+				JButton jb = new JButton();
+				container.add(jb);
+				jb.setBackground(new Color(0x64FF64));
+				Boolean rentre = false;
+				if (i == 0 || i == largeurMax - 1 || j == 0 || j == longueurMax - 1) {
+					jb.setBackground(new Color(0x00FF00));
+					jb.setEnabled(false); // Le bouton n'est plus cliquable
+					rentre = true;
+
+				}
+				if (i == 0 && j == 0) {
+					BoutonMemoire = jb;
+					rentre = true;
+				}
+				if (i == 0 && j == 1) {
+					BoutonTour = jb;
+					rentre = true;
+				}
+				if (i == largeurMax - 1 && j == 6) {
+					jb.setIcon(new ImageIcon(iconNoix.getImage()));
+					jb.setEnabled(true);
+					rentre = true;
+					// JOptionPane.showMessageDialog(null, "Noix ");
+					clickActionGetPlante(jb, new Noix());
+				}
+				if (i == largeurMax - 1 && j == 5) {
+					jb.setIcon(new ImageIcon(iconPoire.getImage()));
+					jb.setEnabled(true);
+					rentre = true;
+					// JOptionPane.showMessageDialog(null, "je suis une pois ");
+					clickActionGetPlante(jb, new Poire());
+				}
+				if (i == largeurMax - 1 && j == 4) {
+					jb.setIcon(new ImageIcon(iconSoleil.getImage()));
+					jb.setEnabled(true);
+					rentre = true;
+					// IC.setImage(getIconImage());
+					// jb.addActionListener(this);
+					clickActionGetPlante(jb, new Soleil());
+
+				}
+				if (!rentre) {
+					CaseData cd = new CaseData();
+					cd.jButton = jb;
+					clickActionSetPlante(cd);
+				}
+			}
+
+		}
+		this.setContentPane(container);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		Game gl = new Game();
+		while(Tour < 100  && Loose == false) {
+			gl.nextTurn();
+			try {
+				TimeUnit.SECONDS.sleep(3);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-
+	
+	public void nextTurn() {
+		Tour++;
+		BoutonTour.setText(Integer.toString(Tour));
+	}
 	public static void clickActionGetPlante(JButton jb, Plante toset) {
 		jb.addMouseListener(new MouseListener() {
 			@Override
